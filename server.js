2d955ba123f74d5b9874704f0a15f3a8d680c058
unknown <agustingmez@gmail.com>
2019-04-30 17:12:52 -0300

    Todo jaja

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+
+app.get('/', (req,res) => {
+  res.render(__dirname + '/view.html')
+})
+
+//---------------------Listen 3000------------------------------------------
+app.listen(3000);
+console.log("Running at Port 3000");
